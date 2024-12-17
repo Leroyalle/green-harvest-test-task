7b56a53a329650bc6844d106e8f39280ef6f9068
Leroyalle <nikolajmelonov@yandex.ru>
2024-12-18 01:27:01 +0300

    refactor: make comment field optional

--- src/components/shared/feedback/forms/schema.ts
+++ src/components/shared/feedback/forms/schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const feedbackFormSchema = z.object({
   fullName: z.string().min(2, { message: 'Не менее 2 символов' }),
   email: z.string().email({ message: 'Введите корректную почту' }),
-  comment: z.string().min(5, { message: 'Не менее 5 символов' }),
+  comment: z.string().max(1024, { message: 'Не более 1024 символов' }).optional(),
 });
 
 export type TFormFeedbackValues = z.infer<typeof feedbackFormSchema>;
